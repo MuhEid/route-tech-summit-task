e9fff329d5f988bf7d266af24b635da64864c53c
MuhEid96 <moh.eid.1996@gmail.com>
2024-07-17 23:10:58 +0300

    refactor: make the get all task api accessible without authentication

--- src/modules/Tasks/task.route.js
+++ src/modules/Tasks/task.route.js
@@ -5,7 +5,7 @@ const route = Router();
 
 route.post("/:categoryId", authMiddleware, taskRouter.addTask);
 route.put("/:taskId", authMiddleware, taskRouter.updateTask);
-route.get("/", authMiddleware, taskRouter.getAllTasks);
-route.delete("/:categoryId", authMiddleware, taskRouter.deleteTask);
+route.get("/", taskRouter.getAllTasks);
+route.delete("/:taskId", authMiddleware, taskRouter.deleteTask);
 
 export default route;
